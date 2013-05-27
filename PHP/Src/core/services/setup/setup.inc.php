95f505aecdeacf5d5faf0f4b36b15134c70c7caa
Johan Strydom <johan@webserv.com.na>
2013-05-27 16:35:00 +0100

    Site Setup Modification

--- PHP/Src/core/services/setup/setup.inc.php
+++ PHP/Src/core/services/setup/setup.inc.php
@@ -67,21 +67,70 @@ class TsamaSetup extends TsamaObject{
 		$form = HTML5Parser::CreateForm($parentNode,'setup/save');
 		//show site form
 		$h3 = $form->AddChild('h3')->SetValue('Site Setup<span class="nl txtNormal">Please enter your site information.</span>');
-		$siteName = HTML5Parser::CreateTextField($form,'Site Name','site','your site name','',TRUE);
-		$ad = HTML5Parser::CreateTextField($form,'Admin Domain','domain','your admin domain','localhost',TRUE);
+
+		$site = '';
+		if(isset($_POST['site'])){ $site = $_POST['site']; }
+		$siteName = HTML5Parser::CreateTextField($form,'Site Name','site','your site name',$site,TRUE);
+
+		$domain = 'localhost';
+		if(isset($_POST['domain'])){ $domain = $_POST['domain']; }
+		$ad = HTML5Parser::CreateTextField($form,'Admin Domain','domain','your admin domain',$domain,TRUE);
 
 		//TODO: Site Administrator
 
 		$h3 = $form->AddChild('h3')->SetValue('Database Setup<span class="nl txtNormal">Please enter your database credentials.</span>');
 
 		//show db form
-		$driver = HTML5Parser::CreateTextField($form,'Driver','driver','your db driver','mysql',TRUE);
-		$host = HTML5Parser::CreateTextField($form,'Host','host','your db host','localhost',TRUE);
-		$uid = HTML5Parser::CreateTextField($form,'Username','uid','your db user','',TRUE);
+		$driver = 'mysql';
+		if(isset($_POST['driver'])){ $driver = $_POST['driver']; }
+		$driver = HTML5Parser::CreateTextField($form,'Driver','driver','your db driver',$driver,TRUE);
+
+		$host = 'localhost';
+		if(isset($_POST['host'])){ $host = $_POST['host']; }
+		$host = HTML5Parser::CreateTextField($form,'Host','host','your db host',$host,TRUE);
+		$uid = '';
+		if(isset($_POST['uid'])){ $uid = $_POST['uid']; }
+		$uid = HTML5Parser::CreateTextField($form,'Username','uid','your db user',$uid,TRUE);
+
 		$pwd = HTML5Parser::CreatePasswordField($form,'Password','pwd','your db user password','',TRUE);
-		$nm = HTML5Parser::CreateTextField($form,'Name','nm','your db name','',TRUE);
+
+		$nm = '';
+		if(isset($_POST['nm'])){ $nm = $_POST['nm']; }
+		$nm = HTML5Parser::CreateTextField($form,'Name','nm','your db name',$nm,TRUE);
 
 		$save = HTML5Parser::CreateButton($form,'Save',TRUE);
+	}
+
+	private function WriteSiteConf(){
+		//assuming files are writable, for now.
+
+		//site
+		$confSite = file_get_contents(Tsama::_conf('BASEDIR').DS.'conf'.DS.'site.conf.xmpl.php');
+
+		$site = $_POST['site'];
+		$domain = $_POST['domain'];
+
+		$conf = "<?php\n\$_TSAMA_CONFIG['NAME'] = '".$site."';\n\$_TSAMA_CONFIG['LOGO'] = 'tsama.png';\n\$_TSAMA_CONFIG['COMPRESS'] = TRUE;\n\$_TSAMA_CONFIG['HIDE_TSAMA'] = TRUE;\n\$_TSAMA_CONFIG['THEME'] = 'default';\n\$_TSAMA_CONFIG['LAYOUT'] = 'default';\n\$_TSAMA_CONFIG['ADMINDOMAIN'] = '".$domain."';\n\$_TSAMA_CONFIG['LANGUAGE'] = 'en';\n\$_TSAMA_CONFIG['DEBUG'] = TRUE;\n?>";
+		
+
+		$fn = Tsama::_conf('BASEDIR').DS.'conf'.DS.'site.conf.php';
+		$fh = fopen($fn, 'w');
+		fwrite($fh, $conf);
+		fclose($fh);
+
+		return TRUE;
+	}
+
+	private function WriteDbConf(){
+		//continue with save
+		$conf = "<?php\n\$_DB['Driver'] = '".$_POST['driver']."';\n\$_DB['Host'] = '".$_POST['host']."';\n\$_DB['Username'] = '".$_POST['uid']."';\n\$_DB['Password'] = '".$_POST['pwd']."';\n\$_DB['Name'] = '".$_POST['nm']."';\n?>";
+
+		$fn = Tsama::_conf('BASEDIR').DS.'conf'.DS.'db.conf.php';
+		$fh = fopen($fn, 'w');
+		fwrite($fh, $conf);
+		fclose($fh);
+
+		return TRUE;
 	}
 
 	public function get($params){
@@ -102,33 +151,21 @@ class TsamaSetup extends TsamaObject{
 			if((count($route) > 0) && ($route[0] == 'setup') && ($route[1] == 'save')){
 
 				//continue with save
-				//TODO: Validata Database connection
+				try{
+					$db = new PDO(strtolower($_POST['driver']).':host='.$_POST['host'].';dbname='.$_POST['nm'], $_POST['uid'],$_POST['pwd']);
 
-				//site
-				$confSite = file_get_contents(Tsama::_conf('BASEDIR').DS.'conf'.DS.'site.conf.xmpl.php');
+					$this->WriteSiteConf();
 
-				//assuming files are writable, for now.
+					$this->WriteDbConf();
 
-				$confSite = str_replace('My Site Name', $_POST['site'], $confSite);
-				$confSite = str_replace('yourtheme', 'default', $confSite);
-				$confSite = str_replace('yourlayout', 'default', $confSite);
-				$confSite = str_replace('admin_domain', $_POST['domain'], $confSite);
+					$brand = $setup->AddChild('div');
+					$logo = $brand->AddChild('img');
+					$logo->attr('src',Tsama::_conf('BASE').'media/visual/images/default.domain/logo/tsama.png');
 
-				$fn = Tsama::_conf('BASEDIR').DS.'conf'.DS.'site.conf.php';
-				$fh = fopen($fn, 'w');
-				fwrite($fh, $confSite);
-				fclose($fh);
-
-				//continue with save
-				$confDB = "<?php \$_DB['Driver'] = '".$_POST['driver']."'; \r\n \$_DB['Host'] = '".$_POST['host']."';\r\n \$_DB['Username'] = '".$_POST['uid']."'; \r\n \$_DB['Password'] = '".$_POST['pwd']."'; \r\n \$_DB['Name'] = '".$_POST['nm']."'; ?>";
-
-				$fn = Tsama::_conf('BASEDIR').DS.'conf'.DS.'db.conf.php';
-				$fh = fopen($fn, 'w');
-				fwrite($fh, $confDB);
-				fclose($fh);
-
-				$h1 = $setup->AddChild('h1')->SetValue('Tsama Setup<span class="nl txtNormal">Thank you, <a href="'.Tsama::_conf('BASE').'">you may now continue to your homepage</a>.</span>');
-
+					$h1 = $setup->AddChild('h1')->SetValue('Tsama Setup<span class="nl txtNormal">Thank you, <a href="'.Tsama::_conf('BASE').'">you may now continue to your homepage</a>.</span>');
+				}catch(PDOException $e) {
+					$this->ShowForm($setup,'Invalid database credentials. Please try again.');
+				}
 			}
 		}
 	}
